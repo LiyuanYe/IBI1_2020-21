201ed3fec54fe20046f2989ce5706d97c0d14c61
LiyuanYe <79960109+LiyuanYe@users.noreply.github.com>
2021-03-10 13:47:00 +0800

    Update collatz.py

--- Practical5/collatz.py
+++ Practical5/collatz.py
@@ -5,7 +5,7 @@ a = 0
 b = 0    # Set "b" as C(n-1)
 c = 1    # Set "c" as Cn
 
-for i in range(1,13):
+for i in range(0,13):
     print(c)  # Print Cn
     a = c     # use "a" as a "reservoir" to transfer the number from "c" to "b"
     c = c + b # Generate C(n+1) for the next print
